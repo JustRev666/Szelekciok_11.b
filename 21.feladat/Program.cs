8e0e8e6b4312059d28b713c4c11ac5c396368e32
kopasz.bela <kopaszbeci@gmail.com>
2026-09-21 10:11:15 +0200

    21 feladat lkész

--- 21.feladat/Program.cs
+++ 21.feladat/Program.cs
@@ -10,6 +10,23 @@ namespace _21.feladat
     {
         static void Main(string[] args)
         {
+            Console.Write("Kérek egy számot: ");
+            int szam = int.Parse(Console.ReadLine());
+
+            if (szam == 0)
+            {
+                Console.WriteLine("A szám nulla");
+            }
+            else if (szam%2 == 0) 
+            {
+                Console.WriteLine("A szám páros");
+            }
+            else
+            {
+                Console.WriteLine("A szám páratlan");
+            }
+
+            Console.ReadKey();
         }
     }
 }
