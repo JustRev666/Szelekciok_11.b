e35e383702f1e87f0666c06d76a107c104717ee2
tanulo <szabo.marcell.1259@dszcbaross.edu.hu>
2026-09-24 11:40:32 +0200

    push

--- 22.feladat/Program.cs
+++ 22.feladat/Program.cs
@@ -10,6 +10,18 @@ namespace _22.feladat
     {
         static void Main(string[] args)
         {
+            Console.Write("Adjon meg egy tetszőleges számot: ");
+            int szam = int.Parse(Console.ReadLine());
+
+            if (szam%3 == 0)
+            {
+                Console.WriteLine($"{szam} osztható 3-mal maradék nélkül");
+            }
+            else
+            {
+                Console.WriteLine($"{szam} nem osztható 3-mal");
+            }
+            Console.ReadKey();  
         }
     }
 }
